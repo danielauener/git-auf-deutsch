57b03e8dcb459bc628c35ffb2eba69c519ae07d1
makanu <matti.kaupenjohann@fh-dortmund.de>
2026-04-26 12:58:32 +0200

    add: pott gezaehe

---
+++ pott.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+git config --local alias.anstechen init
+git config --local alias.abkupfern clone
+git config --local alias.einfahren pull
+git config --local alias.aufladen add
+git config --local alias.hochjagen push
+git config --local alias.gib-ihm 'push --force'
+git config --local alias.kick-ab branch
+git config --local alias.stollen branch
+git config --local alias.klopp-fest commit
+git config --local alias.pfropf-um rebase
+git config --local alias.luenkern diff
+git config --local alias.klopp-zusammen merge
+git config --local alias.bunkern stash
+git config --local alias.papp-drauf tag
+git config --local alias.spring-rueber checkout
+git config --local alias.berichtsheft log
+git config --local alias.lage status
+git config --local alias.kack-an blame
+git config --local alias.nach-draussen remote
